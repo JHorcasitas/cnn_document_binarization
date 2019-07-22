faa0bee282feb780d5f23dfa816d022c4d8b7caa
JHorcasitas <arturo.horcasitas@gmail.com>
2019-07-21 21:37:31 -0500

    data transformations

---
+++ data_ingestion/transform.py
@@ -0,0 +1,28 @@
+import torch
+
+
+class Normalize:
+    """
+    Normalizes by substracting mean and dividing by standard deviation.
+    Normalization is expected to happen before Tensorization
+    """
+    def __init__(self, mean, std):
+        self._std  = std
+        self._mean = mean
+
+    def __call__(self, sample):
+        img = sample['image']
+        img = (img - self._mean) / self._std
+        sample['image'] = img
+        return sample
+
+class Tensorization:
+    """
+    Convert to PyTorch Tensor and adds dummy dimension
+    """
+    def __call__(self, sample):
+        img = sample['image']
+        img = img[None, ...]
+        sample['image']  = torch.from_numpy(img)
+        sample['target'] = torch.tensor(sample['target'])
+        return sample
