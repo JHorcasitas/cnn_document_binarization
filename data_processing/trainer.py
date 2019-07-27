692b7eca1c4e8aa212a1725073540385445a9b86
JHorcasitas <arturo.horcasitas@gmail.com>
2019-07-27 10:16:34 -0500

    add trainer class

---
+++ data_processing/trainer.py
@@ -0,0 +1,33 @@
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+class Trainer:
+
+    def __init__(self, dataloader, model, device):
+        self._model = model
+        self._device = device
+        self._dataloader = dataloader
+    
+        self._model.train()
+        self._model = self._model.to(self._device)
+
+        self._optimizer = optim.Adam(self._model.parameters())
+
+    def train(self):
+        avg_loss = 0
+        for sample in self._dataloader:
+            input  = sample['image'].to(self._device)
+            target = sample['target'].to(self._device)
+            
+            self._optimizer.zero_grad()
+            output = self._model(input)
+            loss = F.binary_cross_entropy_with_logits(output,
+                                                      target,
+                                                      reduction='mean')
+            loss.backward()
+            self._optimizer.step()
+
+            avg_loss += loss.item()
+
+        self.loss = avg_loss / len(self._dataloader)
