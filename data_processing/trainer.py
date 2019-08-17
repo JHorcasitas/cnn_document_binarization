61ebf04d83bcfa96d49e186d0c6f4f8c6b1d9366
JHorcasitas <arturo.horcasitas@gmail.com>
2019-08-17 10:08:11 -0500

    add max num batches to train

--- data_processing/trainer.py
+++ data_processing/trainer.py
@@ -4,29 +4,45 @@ import torch.nn.functional as F
 
 class Trainer:
 
-    def __init__(self, dataloader, model, device, tensorboard):
-        self._tb         = tensorboard 
-        self._model      = model
-        self._device     = device
-        self._dataloader = dataloader
-    
+    def __init__(self,
+                 dataloader,
+                 model,
+                 device,
+                 tensorboard,
+                 epoch,
+                 num_batches=1000):
+        
+        self._epoch = epoch
+        
+        self._tb          = tensorboard 
+        self._model       = model
+        self._device      = device
+        self._dataloader  = dataloader
+        self._num_batches = num_batches
+
         self._model     = self._model.to(self._device).train()
         self._optimizer = optim.Adam(self._model.parameters())
 
+
     def train(self):
         avg_loss = 0
-        for batch, sample in enumerate(self._dataloader):
-            if batch > 500:
+        for batch, (input, target) in enumerate(self._dataloader):
+
+            if batch == self._num_batches:
                 break
-            input  = sample['image'].to(self._device)
-            target = sample['target'].to(self._device)
+
+            input  = input.to(self._device)
+            target = target.to(self._device)
             
             self._optimizer.zero_grad()
             output = self._model(input)
             loss = F.binary_cross_entropy_with_logits(output,
                                                       target,
                                                       reduction='mean')
-            self._tb.add_scalar('Batch Loss', loss.item(), batch)
+            avg_loss += loss.item()
+            self._tb.add_scalar('Batch Loss',
+                                loss.item(),
+                                batch + (self._epoch * self._num_batches))
+            
             loss.backward()
             self._optimizer.step()
-            avg_loss += loss.item()
