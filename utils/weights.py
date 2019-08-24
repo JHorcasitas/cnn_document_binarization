87550f11b8e2bc36957b6310101f51f1565b056e
JHorcasitas <arturo.horcasitas@gmail.com>
2019-08-23 21:56:43 -0500

    add suppport for valset weight computation

--- utils/weights.py
+++ utils/weights.py
@@ -9,24 +9,24 @@ from PIL import Image
 from definitions import MODELS_PATH
 
 
-def get_weights():
+def get_weights(kind):
     """
-    returns the weights used in WeightedRandomSampler
+    Returns the weights used in WeightedRandomSampler
     """
     cfg = config.read_config()
     data_path = cfg['data_path']
-    train_path = os.path.join(data_path, 'target', 'train')
+    input_path = os.path.join(data_path, 'target', kind)
 
     distribution = []
     order = lambda x: int(os.path.splitext(x)[0])
-    for img_name in sorted(os.listdir(train_path), key=order):
-        img_path = os.path.join(train_path, img_name)
+    for img_name in sorted(os.listdir(input_path), key=order):
+        img_path = os.path.join(input_path, img_name)
         img = Image.open(img_path).convert('L')
         img = np.array(img)
         img = img.ravel()
         img = np.where(img > 0, 1, 0)
         distribution.extend(img.tolist())
-    
+
     counts = Counter(distribution)
     weights = [1 / counts[item] for item in distribution]
     return weights
