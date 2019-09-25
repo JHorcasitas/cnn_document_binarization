61cea5785748a499f31c3adc376121c76fe75596
JHorcasitas <arturo.horcasitas@gmail.com>
2019-09-24 21:59:16 -0500

    automatically apply data transformations

--- data_ingestion/dataset_factory.py
+++ data_ingestion/dataset_factory.py
@@ -4,25 +4,27 @@ from .dataset import StridedDataset
 
 def get_dataset(dataset,
                 kind,
-                input_transform=None,
-                target_transform=None):
+                transform_input=False,
+                transform_target=False):
     """
     Retrieves especified dataset
     Args:
         dataset (str): either ´binary´ or ´strided´
         kind (str): one of {train, test, val}
-        input_transform (callable): transformation to apply on input
-        target_transform (callable): transformation to apply on target
+        transform_input (bool): wether to transform target so that it can be
+        processed by PyTorch
+        transform_target (bool): wether to transform target so that it can be
+        processed by PyTorch
     """
     if kind not in {'train', 'test', 'val'}:
         raise ValueError(f'Kind value: \'{kind}\' not supported')
 
     if dataset == 'binary':
         return BinaryDataset(kind=kind,
-                             input_transform=input_transform)
+                             transform_input=transform_input)
     elif dataset == 'strided':
         return StridedDataset(kind=kind,
-                              input_transform=input_transform,
-                              target_transform=target_transform)
+                              transform_input=transform_input,
+                              transform_target=transform_target)
     else:
         raise ValueError(f'Dataset value: \'{dataset}\' not supported')
