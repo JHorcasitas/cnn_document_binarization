bf66e513b3288bb0fa1489c009c63616c40cd181
JHorcasitas <arturo.horcasitas@gmail.com>
2019-09-24 21:59:29 -0500

    general improvements

--- data_ingestion/dataset.py
+++ data_ingestion/dataset.py
@@ -1,41 +1,43 @@
 import os
+import configparser
 from collections import OrderedDict
 
 import numpy as np
 from PIL import Image, ImageOps
 from torch.utils.data import Dataset
 
-import config
+from .data_transformations import input_transform, target_transform
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 class BinaryDataset(Dataset):
     """
     Retrieve image patches
-    """
-    def __init__(self, kind, input_transform=None):
-        """
-        args:
+    args:
         kind (str): one of: {train, test, val}
-        transform (): transformation to apply to every image
-        """
-        cfg = config.read_config()
+        input_transform (callable): transformation to apply to every image
+    """
+    def __init__(self, kind, transform_input=False):
+        self._kind = kind
+        self._radius = config['data_ingestion']['radius']
+        self._data_path = config['data_ingestion']['data_path']
+        self._transform_input = transform_input
 
-        self._kind       = kind
-        self._radius     = cfg['radius']
-        self._data_path  = cfg['data_path']
-        self._input_transform  = input_transform
-        
-        input_path  = os.path.join(self._data_path, 'input', self._kind)
+        input_path = os.path.join(self._data_path, 'input', self._kind)
         target_path = os.path.join(self._data_path, 'target', self._kind)
-        self._input_imgs  = self._load_images(input_path, 'L')
-        self._target_imgs = self._load_images(target_path, '1')
+        self._input_imgs = self._load_images(input_path, mode='L')
+        self._target_imgs = self._load_images(target_path, mode='1')
 
         self._img_pixels = self._get_num_pixels()
-    
+
     def _load_images(self, path, mode):
         img_dict = OrderedDict()
-        order = lambda x: int(os.path.splitext(x)[0])
-        for img_name in sorted(os.listdir(path), key=order):
+        image_list = sorted(os.listdir(path),
+                            key=lambda x: int(os.path.splitext(x)[0]))
+        for img_name in image_list:
             img_path = os.path.join(path, img_name)
             img_dict[img_name] = Image.open(img_path).convert(mode)
         return img_dict
@@ -51,49 +53,43 @@ class BinaryDataset(Dataset):
         return num_pixels
 
     def __len__(self):
-        """
-        Every pixel represents a training pattern
-        """
-        num_patterns = 0
-        for key in self._img_pixels:
-            num_patterns += self._img_pixels[key]
-        return num_patterns
-    
+        """ Every pixel (with its surrounding area) is a training pattern """
+        return sum(self._img_pixels.values())
+
     def __getitem__(self, idx):
 
-        def get_img_info(idx):
+        def get_img_name_and_index(idx):
             total = 0
             for key in self._img_pixels:
                 total += self._img_pixels[key]
                 if idx < total:
                     return key, idx - (total - self._img_pixels[key]) + 1
-            raise RuntimeError(f'Index {idx} could not be found')
-        
+
         def get_coord_from_idx(input_img, img_idx):
             w, h = input_img.size
             row = (img_idx - 1) // w
             col = (img_idx - 1) % w
             return col, row
 
-        img_name, img_index = get_img_info(idx)
-        input_img  = self._input_imgs[img_name] 
+        img_name, img_index = get_img_name_and_index(idx)
+        input_img = self._input_imgs[img_name]
         target_img = self._target_imgs[img_name]
-        
+
         x, y = get_coord_from_idx(input_img, img_index)
         input_img = ImageOps.expand(input_img, border=self._radius, fill=255)
-        
+
         # Get input
-        right  = x + (2 * self._radius) + 1
+        right = x + (2 * self._radius) + 1
         bottom = y + (2 * self._radius) + 1
         input = input_img.crop((x, y, right, bottom))
 
         # Get target
-        target = target_img.getpixel((x, y))              
+        target = target_img.getpixel((x, y))
         target = 1 if target > 0 else 0
         target = np.array(target, dtype=np.float32).reshape(1)
 
-        if self._input_transform:
-            input = self._input_transform(input)
+        if self._transform_input:
+            input = input_transform(input)
 
         return input, target
 
@@ -101,32 +97,32 @@ class BinaryDataset(Dataset):
 class StridedDataset(Dataset):
     """
     Retrieve complete images, useful when training fully convolutional
-    networks 
-    """
-    def __init__(self, kind, input_transform=None, target_transform=None):
-        """
-        args:
+    networks
+    Args:
         kind (str): one of: {train, test, val}
-        transform (): transformation to apply to every image
-        """
+        transform_input (bool): wether to transform input so that it can be
+        processed by PyTorch
+        transform_target (bool): wether to transform target so that it can be
+        processed by Pytorch
+    """
+    def __init__(self, kind, transform_input=False, transform_target=False):
         self._kind = kind
-        self._input_transform  = input_transform
-        self._target_transform = target_transform
+        self._transform_input = transform_input
+        self._transform_target = transform_target
 
-        cfg = config.read_config()
-        self._radius     = cfg['radius']
-        self._data_path  = cfg['data_path']
+        self._radius = config['data_ingestion']['radius']
+        self._data_path = config['data_ingestion']['data_path']
 
-        self._input_path  = os.path.join(self._data_path, 'input', self._kind)
+        self._input_path = os.path.join(self._data_path, 'input', self._kind)
         self._target_path = os.path.join(self._data_path, 'target', self._kind)
 
         order = lambda x: int(os.path.splitext(x)[0])
-        self._input_names  = sorted(os.listdir(self._input_path), key=order)
+        self._input_names = sorted(os.listdir(self._input_path), key=order)
         self._target_names = sorted(os.listdir(self._target_path), key=order)
 
     def __len__(self):
         return len(self._input_names)
-    
+
     def __getitem__(self, idx):
         input_path = os.path.join(self._input_path, self._input_names[idx])
         input = Image.open(input_path).convert('L')
@@ -134,9 +130,9 @@ class StridedDataset(Dataset):
         target_path = os.path.join(self._target_path, self._target_names[idx])
         target = Image.open(target_path).convert('1')
 
-        if self._input_transform:
-            input = self._input_transform(input)
-        if self._target_transform:
-            target = self._target_transform(target)
-    
+        if self._transform_input:
+            input = input_transform(input)
+        if self._transform_target:
+            target = target_transform(target)
+
         return input, target
