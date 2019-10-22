fec7dea122f7ec937c3b8224ea4b5560c49e10e5
JHorcasitas <arturo.horcasitas@gmail.com>
2019-10-21 21:15:23 -0500

    improve pep compliance

--- data_processing/trainer.py
+++ data_processing/trainer.py
@@ -12,31 +12,31 @@ class Trainer:
                  tb=None,
                  num_batches=500):
         """
-        """ 
+        """
         self._dataloader = dataloader
-        self._model      = model.to(device)
-        self._device     = device
-        self._optim      = optimizer
-        
+        self._model = model.to(device)
+        self._device = device
+        self._optim = optimizer
+
         self._tb = tb
         self._num_batches = num_batches
-        
+
         self._epoch = 0
 
     def train(self):
         """
         """
         self._model.train()
-        
+
         avg_loss = 0
         for batch, (input, target) in enumerate(self._dataloader):
 
             if batch == self._num_batches:
                 break
 
-            input  = input.to(self._device)
+            input = input.to(self._device)
             target = target.to(self._device)
-            
+
             self._optim.zero_grad()
             output = self._model(input)
             loss = F.binary_cross_entropy_with_logits(output,
@@ -46,8 +46,8 @@ class Trainer:
             self._tb.add_scalar('Batch Loss',
                                 loss.item(),
                                 batch + (self._epoch * self._num_batches))
-            
+
             loss.backward()
             self._optim.step()
-        
+
         self._epoch += 1
