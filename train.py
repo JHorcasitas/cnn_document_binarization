ac03053f784056f55a5b679273b7f99e6aa61818
JHorcasitas <arturo.horcasitas@gmail.com>
2019-10-21 21:15:42 -0500

    improve pep compliance

--- train.py
+++ train.py
@@ -13,15 +13,16 @@ import data_processing
 from constants import CONFIG_PATH, MODELS_PATH
 from data_ingestion.transform import Normalize, Tensorize
 
+
 # Tensorboard
 runs_dir = '/home/jorge/Documents/Blog/img_bin/runs'
-tb = SummaryWriter(log_dir=os.path.join(runs_dir, 'exp4'))
+tb = SummaryWriter(log_dir=os.path.join(runs_dir, 'adam-bs1500'))
 
 # Data Ingestion
 transform = Compose([Normalize(mean=0.733, std=0.129), Tensorize()])
 dataset_factory = data_ingestion.DatasetFactory()
 dataset = dataset_factory.get_dataset(kind='train', transform=transform)
-dataloader = DataLoader(dataset, batch_size=150, shuffle=True, num_workers=4)
+dataloader = DataLoader(dataset, batch_size=1000, shuffle=True, num_workers=4)
 
 # Load Model
 model = models.BinNet()
