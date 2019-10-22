4f3c3999eaa64596c0a408d4abcf51315335096f
JHorcasitas <arturo.horcasitas@gmail.com>
2019-10-21 21:09:13 -0500

    delete definitions

--- definitions.py
+++
@@ -1,7 +0,0 @@
-import os
-
-
-PACKAGE_PATH = os.path.dirname(__file__)
-CONFIG_PATH  = os.path.join(PACKAGE_PATH, 'config', 'config.ini')
-UTILS_PATH   = os.path.join(PACKAGE_PATH, 'utils')
-MODELS_PATH  = os.path.join(PACKAGE_PATH, 'models', 'cache')
